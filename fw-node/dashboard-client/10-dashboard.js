d1ddce025479e96fb91e33b32b701e66f5031077
Pavel Burgr <pb@drake.cz>
2018-09-19 16:06:07 +0200

    WIP

--- fw-node/dashboard-client/10-dashboard.js
+++ fw-node/dashboard-client/10-dashboard.js
@@ -5,47 +5,67 @@ wg.pages.home = {
         let registers = await wg.dashboard.getRegisters();
 
         function updateRegister(register) {
-            let el = registers[register.key].el;
             
+            let diff;
+
             if (typeof register.value === "number") {
                 register.value = Math.round(register.value * 10) / 10;
-                let diff = register.value - registers[register.key].value;
-                el.toggleClass("goesDown", diff < 0);
-                el.toggleClass("goesUp", diff > 0);
+                diff = register.value - registers[register.key].value;
             }
 
             registers[register.key].value = register.value;            
 
-            el
-            .find(".number")
+            $(".register-bound." + register.key)
             .text(
+                (
                 register.value instanceof Object? 
                     register.value.key? 
                         register.value.key: 
                         JSON.stringify(register.value): 
                     typeof register.value === "number"?
                         register.value.toFixed(1):
-                        register.value
-                );
+                        register.value === undefined? "-": register.value
+                ) + (register.unit? " " + register.unit: "")
+            )
+            .toggleClass("goesDown", diff < 0)
+            .toggleClass("goesUp", diff > 0)
+            ;
+        }
+
+        function updateAllRegisters() {
+            Object.values(registers).forEach(updateRegister);
         }
 
         container.append(
-            SPAN("registers",
-                Object.values(registers).map(register => 
-                    SPAN("register", [    
-                        SPAN("name").text(register.name),
-                        SPAN("value", [
-                            SPAN("number"),
-                            SPAN("unit").text(register.unit)
+            SPAN("dashboard", [
+                DIV("registers",
+                    Object.values(registers).map(register => 
+                        SPAN("register", [    
+                            SPAN("name").text(register.name),
+                            SPAN("value register-bound " + register.key)
                         ])
-                    ], span => {
-                        register.el = span;
-                        updateRegister(register);
-                    })
-                )                
-            ).onRegisterChanged(cr => {
-                updateRegister(cr)
-            })
-        )
+                    )                
+                ).onRegisterChanged(cr => {
+                    updateRegister(cr)
+                }),
+                DIV("schema", span => {
+                    $.get("schema.svg", svg => {
+                        let svgStr = (new window.XMLSerializer()).serializeToString(svg);
+                        span.html(svgStr);
+                        span.find("tspan").each((i, tspan) => {
+                            tspan = $(tspan);
+                            let key = tspan.text();
+                            if (key.startsWith("$")) {
+                                key = key.substring(1);
+                                tspan.addClass("register-bound " + key);
+                            }
+                        });
+                        updateAllRegisters();
+                    });                    
+                })
+            ])
+        );
+
+        updateAllRegisters();
     }
 }
